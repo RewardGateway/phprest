e3fea6a1de74fd5064b86f0051dec231cd9c0577
Ayan Ozturk <ayan.ozturk@rewardgateway.com>
2024-07-15 16:31:28 +0100

    add common controller methods trait to handle serialisation

--- tests/Mock/Controller/TestController.php
+++ tests/Mock/Controller/TestController.php
@@ -2,14 +2,16 @@
 
 namespace Phprest\Test\Mock\Controller;
 
-use Phprest\Response\Ok;
 use Phprest\Util\Controller as AbstractController;
 use Phprest\Test\Mock\Entity\TestResponse;
+use Symfony\Component\HttpFoundation\Response;
 
 class TestController extends AbstractController
 {
-    public function testAction(): Ok
+    public function testAction(): Response
     {
-        return new Ok(new TestResponse('Hello Phprest World'));
+        return $this->okResponse(
+            new TestResponse('Hello Phprest World')
+        );
     }
 }
